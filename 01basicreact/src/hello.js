6acf045ad65399b640849797afba0e5d7881e181
Radheya-2811 <dubeyh1414@gmail.com>
2024-07-15 01:22:16 +0530

    bgChanger

--- 01basicreact/src/hello.js
+++
@@ -1,7 +0,0 @@
-function hello(){
-    return(
-        <h2>Hello function !!!!</h2>
-    )
-}
-
-export default hello
--- 01vitereact/src/f1.jsx
+++
@@ -1,7 +0,0 @@
-function F1(){
-    return(
-        <h2>This is a user defined function</h2>
-    )
-}
-
-export default F1
